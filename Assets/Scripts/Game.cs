f1c3d5576c6f4f4c7a6b5d4e375f6424e52ef8fa
Thomas Van Gysegem <thomasvangysegem@gmail.com>
2019-11-21 18:20:31 +0100

    [add] Main menu

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonoBehaviourTest : MonoBehaviour
+public class Game : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("coucou");
+        
     }
 
     // Update is called once per frame
