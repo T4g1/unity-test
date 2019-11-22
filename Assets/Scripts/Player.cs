80e10b7cfcd66a5c0439ac7a901c46681de04f69
Thomas Van Gysegem <thomasvangysegem@gmail.com>
2019-11-22 01:28:08 +0100

    [fix] Removed smooth inputs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,23 +5,69 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D body;
+    public SpriteRenderer sprite;
     public Animator animator;
     public float speed = 8.0f;
 
+    Vector3 moveDirection = Vector3.zero;
+    bool isDead = false;
+    string lastAnimation = "";
+
+
+    void Update()
+    {
+        UpdateAnimator();
+    }
+
     void FixedUpdate()
     {
-        Vector3 moveDirection = new Vector3(
-            Input.GetAxis("Horizontal"), 
-            Input.GetAxis("Vertical"),
-            0.0f
+        moveDirection = new Vector2(
+            Input.GetAxisRaw("Horizontal"), 
+            Input.GetAxisRaw("Vertical")
         );
-        Vector3 movement = moveDirection * speed * Time.fixedDeltaTime;
         
-        body.AddForce(movement);
+        body.velocity = moveDirection * speed * Time.fixedDeltaTime;
+    }
 
-        animator.SetFloat("vertical_move", moveDirection.y);
-        animator.SetFloat("horizontal_move", moveDirection.x);
+    void UpdateAnimator()
+    {
+        if (isDead) {
+            sprite.flipX = false;
+            SetAnimation("Player_die");
+        }
+        if (moveDirection.y < 0.0f) {
+            sprite.flipX = false;
+            SetAnimation("Player_walk_down");
+        }
+        else if (moveDirection.y > 0.0f) {
+            sprite.flipX = false;
+            SetAnimation("Player_walk_up");
+        }
+        else if (moveDirection.x > 0.0f) {
+            sprite.flipX = false;
+            SetAnimation("Player_walk_right");
+        }
+        else if (moveDirection.x < 0.0f) {
+            sprite.flipX = true;
+            SetAnimation("Player_walk_right");
+        }
 
-        animator.SetBool("moving", moveDirection.magnitude > 0.0f);
+        else if (lastAnimation == "Player_walk_up") {
+            sprite.flipX = false;
+            SetAnimation("Player_idle_up");
+        }
+        else if (lastAnimation == "Player_walk_down") {
+            sprite.flipX = false;
+            SetAnimation("Player_idle_down");
+        }
+        else if (lastAnimation == "Player_walk_right") {
+            SetAnimation("Player_idle_right");
+        }
+    }
+
+    void SetAnimation(string stateName)
+    {
+        animator.Play(stateName);
+        lastAnimation = stateName;
     }
 }
